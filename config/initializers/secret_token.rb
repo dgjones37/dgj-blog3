ca9072b6529acf800ed2919af939ccd10f4cc17c
Donald Jones <other214@gmail.com>
2013-07-18 17:23:28 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DgjBlog3::Application.config.secret_token = '9dc3ec5c2eedb28e17867ba713d6be57915c75fc2a4863b7b207fc223564d1e061d660daf821796971079db4b0ee3ddf59b4d46f5fc29a9bd24fdb535574b3e7'
